92caf122f40abda3c88b6cd99a6262969fda53bd
hatimb03 <hatim.ba53@gmail.com>
2024-07-03 10:06:54 +0530

    Put and patch implementation of menuItems

--- routes/menuItemRoutes.js
+++ routes/menuItemRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const MenuItem = require("../models/MenuItem.js");
+const MenuItem = require("./../models/MenuItem.js");
 
 router.post("/", async (req, res) => {
   try {
@@ -44,6 +44,44 @@ router.get("/:tasteType", async (req, res) => {
     }
   } catch (err) {
     res.status(500).json({ error: "internal server error" });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updatedPersonData = req.body;
+    const response = await MenuItem.findByIdAndUpdate(id, updatedPersonData, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!response) {
+      res.status(404).json({ error: "Person not found" });
+    }
+
+    console.log("Data updated");
+    res.status(200).json(response);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const response = await MenuItem.findByIdAndDelete(id);
+
+    if (!response) {
+      res.status(404).json({ error: "Menu Item not found" });
+    }
+
+    console.log("Menu Item deleted");
+    res.status(200).json({ message: "Data deleted" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ Error: "Internal server error" });
   }
 });
 
